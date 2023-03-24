d9623e99e591dfe34d5ae17f03a7a57d4b8419c7
Andrew Craswell <ajcraswell@gmail.com>
2023-03-23 22:07:11 -0700

    Enable husky and lint-staged

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from "react"
 
 export default function Web() {
-  const [myVal, setMyVal] = useState<string>("true");
+  const [myVal, setMyVal] = useState<string>("true")
 
   return (
     <div>
       <h1>Web</h1>
     </div>
-  );
+  )
 }
--- packages/eslint-config-piste/index.js
+++ packages/eslint-config-piste/index.js
@@ -1,11 +1,13 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
+  plugins: ["prettier"],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
   },
-};
+  rules: {
+    "prettier/prettier": "error",
+    "@next/next/no-html-link-for-pages": "off",
+  },
+}
